a5c0e35163a7f2844cd566e3cabc7897751afc89
Barry Roberts <barryjroberts@gmail.com>
2023-09-24 22:46:22 +0900

    cleaning

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,47 +9,32 @@
 require 'dotenv/load'
 api_key = ENV['TMDB_API_KEY']
 
-require "nokogiri"
-# require "open-uri"
-file = "meguro.html"
-doc = Nokogiri::HTML.parse(File.open(file), nil, "shift-JIS")
+require 'nokogiri'
+# require 'open-uri'
+file = 'meguro.html'
+doc = Nokogiri::HTML.parse(File.open(file), nil, 'shift-JIS')
 
 Movie.destroy_all
 Cinema.destroy_all
-puts "Movies & Cinemas deleted"
+puts 'Movies & Cinemas deleted'
 
 require 'date'
 
-# movie_1 = Movie.new(
-#   name: "Fargo",
-#   language: "English",
-#   runtime: 120
-# )
-
-# movie_1.save
-
-# movie_2 = Movie.new(
-#   name: "Bait",
-#   language: "Welsh",
-#   runtime:  115
-# )
-
-# movie_2.save
 
 cinema_1 = Cinema.new(
-  name: "Meguro Cinema",
-  location: "〒141-0021 東京都品川区上大崎２丁目２４−１５ 朝日建物株式会社 目黒西口ビル B1",
-  url: "http://www.okura-movie.co.jp/meguro_cinema/now_showing.html",
-  description: "A small, single screen cinema showing old and new movies."
+  name: 'Meguro Cinema',
+  location: '〒141-0021 東京都品川区上大崎２丁目２４−１５ 朝日建物株式会社 目黒西口ビル B1',
+  url: 'http://www.okura-movie.co.jp/meguro_cinema/now_showing.html',
+  description: 'A small, single screen cinema showing old and new movies.'
 )
 
 cinema_1.save
 
 cinema_2 = Cinema.new(
-  name: "Kawasaki Art Centre",
-  location: "〒215-0004 神奈川県川崎市麻生区万福寺６丁目７−１",
-  url: "https://kac-cinema.jp/",
-  description: "A cinema with many European movies."
+  name: 'Kawasaki Art Centre',
+  location: '〒215-0004 神奈川県川崎市麻生区万福寺６丁目７−１',
+  url: 'https://kac-cinema.jp/',
+  description: 'A cinema focused on European movies.'
 )
 
 cinema_2.save
@@ -72,7 +57,6 @@ cinema_2.save
 # end
 # end
 
-
 # def scrape(keyword)
 #   attr_reader :search_results
 
@@ -92,41 +76,35 @@ not_found = []
 
 @search_results.uniq.each { |scraped_title|
 
-encoded_title = URI.encode_www_form_component(scraped_title)
+  encoded_title = URI.encode_www_form_component(scraped_title)
+  url = URI("https://api.themoviedb.org/3/search/movie?api_key=#{api_key}&query=#{encoded_title}&language=en-gb")
+  response = Net::HTTP.get(url)
+  movie_data = JSON.parse(response)
 
-url = URI("https://api.themoviedb.org/3/search/movie?api_key=#{api_key}&query=#{encoded_title}&language=en-gb")
+  if movie_data['results'].any?
+    title = movie_data['results'][0]['title']
+    overview = movie_data['results'][0]['overview']
+    language = movie_data['results'][0]['original_language']
+    poster = movie_data['results'][0]['poster_path']
+    id = movie_data['results'][0]['id']
+    credits_url = URI("https://api.themoviedb.org/3/movie/#{movie_data['results'][0]['id']}/credits?api_key=#{api_key}")
+    credits_response = Net::HTTP.get(credits_url)
+    credits_data = JSON.parse(credits_response)
 
-response = Net::HTTP.get(url)
-movie_data = JSON.parse(response)
-
-if movie_data['results'].any?
-  title = movie_data['results'][0]['title']
-  overview = movie_data['results'][0]['overview']
-  language = movie_data['results'][0]['original_language']
-  poster = movie_data['results'][0]['poster_path']
-  id = movie_data['results'][0]['id']
-  credits_url = URI("https://api.themoviedb.org/3/movie/#{movie_data['results'][0]['id']}/credits?api_key=#{api_key}")
-  credits_response = Net::HTTP.get(credits_url)
-  credits_data = JSON.parse(credits_response)
-
-  runtime_url = URI("https://api.themoviedb.org/3/movie/#{id}?&append_to_response=videos&api_key=#{api_key}")
-  runtime_response = Net::HTTP.get(runtime_url)
-  detailed_data = JSON.parse(runtime_response)
-
+    runtime_url = URI("https://api.themoviedb.org/3/movie/#{id}?&append_to_response=videos&api_key=#{api_key}")
+    runtime_response = Net::HTTP.get(runtime_url)
+    detailed_data = JSON.parse(runtime_response)
     runtime = detailed_data['runtime']
-  director = credits_data['crew'].find { |person| person['job'] == 'Director' }['name']
+    director = credits_data['crew'].find { |person| person['job'] == 'Director' }['name']
 
-  new_movie = Movie.new(director: director, runtime: runtime, name: title, description: overview, web_title: scraped_title, language: language, poster: "https://image.tmdb.org/t/p/w185/#{poster}")
-  if new_movie.save
-    puts "#{title} saved successfully"
+    new_movie = Movie.new(director: director, runtime: runtime, name: title, description: overview,
+      web_title: scraped_title, language: language, poster: "https://image.tmdb.org/t/p/w185/#{poster}")
+    puts new_movie.save ? "#{title} saved successfully" : 'Error when saving---------------------------------------'
   else
-    puts "Error when saving-----------------------------------------------------------------------"
+    not_found << scraped_title
+    puts 'Movie not found'
   end
-else
-  not_found << scraped_title
-  puts "Movie not found"
-end
 }
 
 not_found.each { |x| puts "#{x} not found" }
-puts not_found.size > 0 ? "#{not_found.size} movies not found in total" : "All movies found"
+puts not_found.size.positive? ? "#{not_found.size} movies not found in total" : "All movies found"
