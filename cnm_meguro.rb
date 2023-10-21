a34b96a75e54aa4cb47bfc51eab8bba803eae206
Barry Roberts <barryjroberts@gmail.com>
2023-10-21 18:10:37 +0900

    not fixed yet - check dates with only one instance

--- cnm_meguro.rb
+++ cnm_meguro.rb
@@ -9,13 +9,10 @@ require "date"
 require "nokogiri"
 
 def date(date_string)
-  p date_string.include?("〜")
   if date_string.include?("〜")
     date_ranges = date_string.scan(/(\d{1,2})月(\d{1,2})日/)
     start_date = Date.new(Date.today.year, date_ranges[0][0].to_i, date_ranges[0][1].to_i)
-
     end_date = Date.new(Date.today.year, date_ranges[-1][0].to_i, date_ranges[-1][1].to_i)
-
     (start_date..end_date).map do |date|
       "#{date.month}月#{date.day}日(#{date.strftime("%a")})"
     end
@@ -25,22 +22,23 @@ def date(date_string)
 
     date_ranges.map do |matches|
       start_month = matches[0].to_i
+      p start_month
       start_day = matches[1].to_i
-
+      p start_day
       "#{start_month}月#{start_day}日(#{Date.new(Date.today.year, start_month, start_day).strftime("%a")})"
     end
   end
 end
 
-file = "meguro6.html"
+file = "meguro7.html"
 doc = Nokogiri::HTML.parse(File.open(file), nil, "shift-JIS")
 result = []
 
 doc.search("#timetable").each do |line|
-  # p (line.css("p")).text
   dates = date(line.css("p").text)
   line.css(".time_box tr").each do |row|
     title = row.css(".time_title").text.strip
+    puts title
     times = row.css(".time_type2").map { |el| el.text.strip }
     times.each do |time|
       start_time = time.match(/(0?[0-9]|1[0-9]|2[0-3]):[0-5][0-9]/)
@@ -62,7 +60,8 @@ end
 # end
 
 # puts result.select { |movie| movie[:name] == "プリシラ" }
-puts result.sort_by { |movie| movie[:name] }
+# puts result.sort_by { |movie| movie[:name] }
+puts result
 puts "There are #{result.size} entries"
 
 # To change the dates to Date objects
