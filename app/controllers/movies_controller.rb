1eb4112f3a7ef73b34f45e2ca947d48ddb9ba71f
Barry Roberts <barryjroberts@gmail.com>
2023-10-23 16:46:59 +0900

    wip on movie pages

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,12 @@
 class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+    @showings = Showing.all
+    @cinema = Cinema.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+    @showings = Showing.all.where(movie_id: @movie)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  get "pages/home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "pages#home"
+  resources :movies, only: [:show, :index]
+  resources :cinemas, only: [:show, :index]
   # Defines the root path route ("/")
   # root "articles#index"
 end
