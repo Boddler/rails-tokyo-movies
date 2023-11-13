6a38b85dbfa472d863b8746c842fde370d5347a5
Barry Roberts <barryjroberts@gmail.com>
2023-11-13 16:52:53 +0900

    movie page added showings

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,11 +2,12 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
     @showings = Showing.all
-    @cinema = Cinema.all
+    @cinemas = Cinema.all
   end
 
   def show
     @movie = Movie.find(params[:id])
     @showings = Showing.all.where(movie_id: @movie)
+    @cinemas = @showings.map(&:cinema).uniq
   end
 end
