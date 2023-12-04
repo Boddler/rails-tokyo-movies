f6c897f876f2a9caca84d01696f21a9403b27247
Barry Roberts <barryjroberts@gmail.com>
2023-12-05 05:21:31 +0900

    wip stimulus search

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -4,10 +4,25 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 static targets = ["input", "results"];
 
+connect() {
+  this.movies = JSON.parse(this.data.get("movies"));
+  console.log(this.movies)
+}
+
+
   search(event) {
   event.preventDefault();
   console.log("Search submitted")
   console.log(this.inputTarget.value)
+
+  const searchTerm = this.inputTarget.value.toLowerCase();
+  console.log(searchTerm)
+
+  const filteredMovies = this.movies.filter(movie =>
+    movie.name.toLowerCase().includes(searchTerm)
+    );
+    console.log(filteredMovies)
+
   // debugger
 }
 }
