1e33a423a98e6092bbbc241504631ddb49d424e0
Barry Roberts <barryjroberts@gmail.com>
2023-12-16 17:45:40 +0900

    cinema pages added

---
+++ app/controllers/cinemas_controller.rb
@@ -0,0 +1,11 @@
+class CinemasController < ApplicationController
+  def index
+    @cinemas = Cinema.all
+  end
+
+  def show
+    @cinema = Cinema.find(params[:id])
+    @showings = Showing.all.where(cinema_id: @cinema)
+    @movies = @showings.map(&:movie).uniq
+  end
+end
