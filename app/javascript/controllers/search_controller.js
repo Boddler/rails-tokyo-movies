5c12247cfa5233ea4c36c3e1727898ead87d2d03
Barry Roberts <barryjroberts@gmail.com>
2024-02-08 20:12:00 +0900

    cleaned up the js controller a tiny bit

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -5,9 +5,7 @@ export default class extends Controller {
 static targets = ["input", "results"];
 
 connect() {
-  console.log("Class:", typeof("movies"));
   this.movies = JSON.parse(this.data.get("movies"));
-  console.log("Class:", typeof(this.movies));
   console.log("Movies array:", this.movies);
   console.log("Connected to search controller.");
 }
