dcdd92bc66c4f9c9466cb96ce8a52cf433824e12
Barry Roberts <barryjroberts@gmail.com>
2024-03-08 15:42:55 +0900

    bug fix

--- app/controllers/update_controller.rb
+++ app/controllers/update_controller.rb
@@ -6,10 +6,10 @@ class UpdateController < ApplicationController
     api_results = first_api_call(titles)
     @titles = api_results
     group_call(api_results)
-    Movie.includes(:showings).where(showings: { id: nil }).destroy_all
     times = showings(Cinema.all.first)
     showing_create(times, Cinema.all.first)
     Showing.where("date < ?", Date.today).destroy_all
+    Movie.includes(:showings).where(showings: { id: nil }).destroy_all
     redirect_to root_path
   end
 end
