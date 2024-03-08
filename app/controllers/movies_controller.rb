f66e0dc5fb11fa2c98adac14c10554304173d6a4
Barry Roberts <barryjroberts@gmail.com>
2024-03-08 21:45:48 +0900

    wip still on editing movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,8 @@
+require "net/http"
+
 class MoviesController < ApplicationController
+  include UpdateHelper
+
   def index
     @movies = Movie.all
   end
@@ -7,7 +11,25 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
-  def update
+  def edit
     @movie = Movie.find(params[:id])
+    options = first_api_call([@movie.web_title])[0][0]
+    @temps = temp_movies(options).reject! { |movie| movie.description == @movie.description }
+    raise
+  end
+
+  private
+
+  def temp_movies(list)
+    results = []
+    list.each do |movie|
+      temp_movie = Movie.new(name: movie["title"], description: movie["overview"],
+                             year: movie["release_date"], language: movie["language"],
+                             poster: "https://image.tmdb.org/t/p/w185/#{movie["poster_path"]}",
+                             runtime: movie["id"].to_i)
+      results << temp_movie
+      #  Note - TMDB id is being saved as the runtime
+    end
+    results
   end
 end
