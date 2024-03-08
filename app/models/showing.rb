e86acc95983887e29df9bab85e705c2ed268f80b
Barry Roberts <barryjroberts@gmail.com>
2024-03-08 15:50:14 +0900

    added duplicate showing validation

--- app/models/showing.rb
+++ app/models/showing.rb
@@ -1,5 +1,13 @@
 class Showing < ApplicationRecord
+  validate :unique_attributes
   belongs_to :cinema
   belongs_to :movie
   # validates :datetime, presence: true
+
+  def unique_attributes
+    existing_record = Showing.where(cinema_id: self.cinema_id, date: self.date, times: self.times).first
+    if existing_record && existing_record.id != self.id
+      errors.add(:base, "Another record with the same attributes exists")
+    end
+  end
 end
