9068cef2d8ae0be99cf2d0e12c6e8926489a383e
Barry Roberts <barryjroberts@gmail.com>
2024-03-08 15:37:19 +0900

    clean u

--- app/controllers/update_controller.rb
+++ app/controllers/update_controller.rb
@@ -2,13 +2,12 @@ class UpdateController < ApplicationController
   include UpdateHelper
 
   def update
-    # Showing.where("date < ?", Date.today).destroy_all
     titles = scrape(Cinema.all.first)
     api_results = first_api_call(titles)
     @titles = api_results
     group_call(api_results)
-    # Movie.includes(:showings).where(showings: { id: nil }).destroy_all
-    # raise
-    # redirect_to root_path
+    Showing.where("date < ?", Date.today).destroy_all
+    Movie.includes(:showings).where(showings: { id: nil }).destroy_all
+    redirect_to root_path
   end
 end
