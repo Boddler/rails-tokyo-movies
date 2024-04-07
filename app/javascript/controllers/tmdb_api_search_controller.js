9517f06c1ed7ba23dc80c2857698a5d419b004b1
Barry Roberts <barryjroberts@gmail.com>
2024-04-07 10:59:24 +0900

    nearly there

--- app/javascript/controllers/tmdb_api_search_controller.js
+++ app/javascript/controllers/tmdb_api_search_controller.js
@@ -2,19 +2,50 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="tmdb-api-search"
 export default class extends Controller {
-  static targets = ["newMovie", "input"];
+  static targets = ["newMovie", "input"]
 
   connect() {
     const apiKey = document.querySelector("meta[name='api-key']").getAttribute("content");
-    // console.log(apiKey);
+    console.log(apiKey);
     console.log("Connected!");
   }
 
   search() {
-    console.log("Connected at the search bit too!");
-    // this.newMovieTarget.innerText
-    debugger
-    console.log(this.inputTarget.value);
+    const apiKey = document.querySelector("meta[name='api-key']").getAttribute("content");
+    fetch(`https://api.themoviedb.org/3/movie/${this.inputTarget.value}?api_key=${apiKey}`)
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+      this.addMovie(data);
+    })
+}
+
+addMovie(data) {
+  const searchResultsElement = this.newMovieTarget;
+  console.log("OK?");
+  console.log(data.title);
+  searchResultsElement.innerHTML = `
+  <div class="movie-card">
+    <div class="movie-poster">
+      <img src="https://image.tmdb.org/t/p/w185/${data.backdrop_path}" alt="Movie Poster" style="height: 171px;">
+    </div>
+  </a>
+  <div class="movie-card-text">
+    <div class="movie-card-heading">
+      <div>
+        <h4>${data.title}</h4>
+      </div>
+      <div class="language-tag">
+        ${data.spoken_languages[0].english_name}
+      </div>
+    </div>
+    <div>
+      <p>Release Date: ${data.release_date}</p>
+    </div>
+  </div>
+</div>
+`;
+// debugger
 }
 
 }
