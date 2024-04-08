baf3e9d824fdb4eed871cad847491cd9b68e6122
Barry Roberts <barryjroberts@gmail.com>
2024-04-08 22:14:12 +0900

    super early, passing to desktop

--- app/controllers/update_controller.rb
+++ app/controllers/update_controller.rb
@@ -2,15 +2,16 @@ class UpdateController < ApplicationController
   include UpdateHelper
 
   def update
-    cinemas = Cinema.all
+    # cinemas = Cinema.all
+    cinemas = [Cinema.last]
     titles = scrape(cinemas)
     api_results = first_api_call(titles)
     unsaved_models = group_call(api_results)
     movies_create(unsaved_models)
     @times = showings(cinemas)
     showing_create(@times)
-    Showing.where("date < ?", Date.today).destroy_all
-    Movie.includes(:showings).where(showings: { id: nil }).destroy_all
+    # Showing.where("date < ?", Date.today).destroy_all
+    # Movie.includes(:showings).where(showings: { id: nil }).destroy_all
     redirect_to root_path
   end
 end
--- new_cinema.rb
+++ new_cinema.rb
@@ -26,7 +26,7 @@ def clean_titles(list)
   end
 end
 
-def shimo_dates(string)
+def cinema_dates(string)
   dates = []
   month = string.split("/").first.to_i
   match_data = string.match(/\/(.+)$/)
@@ -51,7 +51,7 @@ def shimo_dates(string)
   dates
 end
 
-def shimo_showings(doc)
+def cinema_showings(doc)
   result = []
   doc.search(".box").each do |table|
     date_text = table.search(".day").text
@@ -72,49 +72,81 @@ end
 
 checking = []
 
-html.search(".box").each do |element|
-  unless element.search(".day").first.nil?
-    if element.search(".day").first.text.strip.include?("\n")
-      element.search(".day").first.text.strip.split("\n").each do |snippet|
-        hash = {}
-        hash[:date] = shimo_dates(snippet)
-        hash[:title] = snippet.search(".eiga-title").first.text.strip unless element.search(".eiga-title").first.nil?
-        hash[:time] = snippet.first.text.strip.match(/\d{1,2}：\d{2}/)
-        checking << hash
+html.search(".schedule-box-txt h1").each do |element|
+  checking << element.text.strip
+end
+
+def movie_api_call(list)
+  api_key = ENV["TMDB_API_KEY"]
+  checking = []
+  languages = JSON.parse(ENV["LANGUAGES"])
+  list = clean_titles(list)
+  list.uniq.each { |scraped_title|
+    cast = []
+    encoded_title = URI.encode_www_form_component("\"#{scraped_title}\"") # checks the quoted title - more precise but might miss stuff?
+    url = URI("https://api.themoviedb.org/3/search/movie?api_key=#{api_key}&query=#{encoded_title}&language=en-gb")
+    response = Net::HTTP.get(url)
+    movie_json = JSON.parse(response)
+    movie_data = movie_json["results"]
+    movie_data = movie_data.sort_by { |movie| -movie["vote_count"].to_f }
+    if movie_data.any?
+      title = movie_data[0]["title"]
+      overview = movie_data[0]["overview"]
+      # language = movie_data.spoken_languages[0]
+      poster = movie_data[0]["poster_path"]
+      language = languages.fetch(movie_data[0]["original_language"], movie_data[0]["original_language"])
+      year = movie_data[0]["release_date"]
+      id = movie_data[0]["id"]
+      popularity = movie_data[0]["popularity"]
+      credits_url = URI("https://api.themoviedb.org/3/movie/#{movie_data[0]["id"]}/credits?api_key=#{api_key}")
+      credits_response = Net::HTTP.get(credits_url)
+      credits_data = JSON.parse(credits_response)
+
+      runtime_url = URI("https://api.themoviedb.org/3/movie/#{id}?&append_to_response=videos&api_key=#{api_key}")
+      runtime_response = Net::HTTP.get(runtime_url)
+      detailed_data = JSON.parse(runtime_response)
+      runtime = detailed_data["runtime"]
+      director = (credits_data["crew"].find { |person| person["job"] == "Director" }.nil? ? "Unknown" : credits_data["crew"].find { |person| person["job"] == "Director" }["name"])
+
+      x = 0
+      10.times do
+        cast << credits_data["cast"][x]["name"] if credits_data["cast"][x] && credits_data["cast"][x]["name"]
+        x += 1
       end
+      background_url = URI("https://api.themoviedb.org/3/movie/#{id}/images?api_key=#{api_key}")
+      background_response = Net::HTTP.get(background_url)
+      background_data = JSON.parse(background_response)
+      # background = (background_data["backdrops"][0].nil? ? "https://www.themoviedb.org/t/p/original/bm2pU9rfFOhuHrzMciV6NlfcSeO.jpg" : background_data["backdrops"][0])
+      background = (background_data["backdrops"].nil? ? nil : background_data["backdrops"])
+      # puts "#{title} found successfully"
     else
-      hash = {}
-      hash[:title] = element.search(".eiga-title").first.text.strip unless element.search(".eiga-title").first.nil?
-      time = element.search(".day").first.text.strip.match(/\d{1,2}：\d{2}/)
-      hash[:times] = time[0] if time
-      hash[:date] = element.search(".day").first.text.strip # unless element.search(".eiga-title").first.text.strip.nil?
-      checking << hash
+      puts "Movie not found: #{scraped_title}"
     end
-  end
+  }
 end
 
-checking.each do |cell|
-  p cell[:title] unless cell[:title].nil?
-  p cell[:times] unless cell[:title].nil?
-  p cell[:date] unless cell[:title].nil?
-  p "*" * 40 unless cell[:title].nil?
-end
+# movie_api_call(checking)
+pp checking
 
-string = "3/16(土)〜3/22(金) 16：05〜(終18：11)\n" + "\t\t  3/23(土)〜3/29(金) 12：05〜(終14：11)"
+# pp checking.drop(5).take(5)
 
-# pp shimo_dates(string)
-
-dates = ["3/23(土) 14:30〜(終15:56)\n\t\t\t3/24(日) 16:20〜(終17:46)\n\t\t\t3/28(木) 14:30〜(終15:56)\n\t\t\t3/29(金) 16:10〜(終17:36)"]
-
-dates.each do |date|
-  date.split("\n").each do |element|
-    shimo_dates(element)
-  end
-end
-
-html.search(".box").each do |element|
-  time = element.search(".day").first.text.strip.match(/\d{1,2}：\d{2}/)
-  # pp time[0] if time
-end
-
-pp checking.drop(5).take(5)
+# html.search(".schedule-txt-catch").each do |element|
+#   unless element.search(".day").first.nil?
+#     if element.search(".day").first.text.strip.include?("\n")
+#       element.search(".day").first.text.strip.split("\n").each do |snippet|
+#         hash = {}
+#         hash[:date] = shimo_dates(snippet)
+#         hash[:title] = snippet.search(".eiga-title").first.text.strip unless element.search(".eiga-title").first.nil?
+#         hash[:time] = snippet.first.text.strip.match(/\d{1,2}：\d{2}/)
+#         checking << hash
+#       end
+#     else
+#       hash = {}
+#       hash[:title] = element.search(".eiga-title").first.text.strip unless element.search(".eiga-title").first.nil?
+#       time = element.search(".day").first.text.strip.match(/\d{1,2}：\d{2}/)
+#       hash[:times] = time[0] if time
+#       hash[:date] = element.search(".day").first.text.strip # unless element.search(".eiga-title").first.text.strip.nil?
+#       checking << hash
+#     end
+#   end
+# end
