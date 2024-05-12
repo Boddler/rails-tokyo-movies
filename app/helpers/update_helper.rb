89a6dcef7ae0a5708e0110c5de838373dc4fdb4e
Barry Roberts <barryjroberts@gmail.com>
2024-05-12 11:20:37 +0900

    fixed dupes on unkown movies being saved

--- app/helpers/update_helper.rb
+++ app/helpers/update_helper.rb
@@ -114,7 +114,8 @@ module UpdateHelper
         existing_movie = Movie.find_by(tmdb_id: new_movie.tmdb_id)
         if existing_movie
           existing_movie.web_title += new_movie.web_title unless existing_movie.web_title.include?(new_movie.web_title[0])
-          existing_movie.save
+          existing_movie.update(web_title: existing_movie.web_title)
+          # existing_movie.save
         else
           Rails.logger.error("Existing movie not found for tmdb_id: #{new_movie.tmdb_id}")
         end
@@ -123,9 +124,9 @@ module UpdateHelper
   end
 
   def unfound_movies(movies)
-    existing_web_titles = Movie.pluck(:web_title).to_set
+    existing_web_titles = Movie.pluck(:web_title).flatten.to_set
     new_movies = movies.map do |movie|
-      unless existing_web_titles.include?(movie)
+      unless existing_web_titles.include?([movie])
         Movie.new(
           director: "Unknown",
           popularity: 0.0,
