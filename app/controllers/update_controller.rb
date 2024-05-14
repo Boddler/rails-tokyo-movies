77d80a75c845b9ab5c2b3639c6ef2ce30a3a9fd8
Barry Roberts <barryjroberts@gmail.com>
2024-05-14 11:43:45 +0900

    think I fixed the dupes and made calls more efficient

--- app/controllers/update_controller.rb
+++ app/controllers/update_controller.rb
@@ -3,14 +3,17 @@ class UpdateController < ApplicationController
 
   def update
     cinemas = Cinema.all
+    movies = Movie.all
     # cinemas = [Cinema.find_by_name("Waseda Shochiku")]
     titles = scrape(cinemas)
     api_results = first_api_call(titles)
     unsaved_models = group_call(api_results[0])
-    movies_create(unsaved_models)
-    unfound_movies(api_results[1])
-    @times = showings(cinemas)
-    showing_create(@times)
+    movies_create(unsaved_models, movies)
+    movies = Movie.all
+    unfound_movies(api_results[1], movies)
+    movies = Movie.all
+    times = showings(cinemas)
+    showing_create(times, movies)
     Showing.where("date < ?", Date.today).destroy_all
     Movie.includes(:showings).where(showings: { id: nil }).destroy_all
     redirect_to root_path
