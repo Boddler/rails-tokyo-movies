0eb7514e8b327796769e97ea9d6ab4a809f31bfc
Barry Roberts <barryjroberts@gmail.com>
2024-07-01 21:56:32 +0900

    menu btn wip

---
+++ app/javascript/controllers/menu_btn_controller.js
@@ -0,0 +1,10 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="menu-btn"
+export default class extends Controller {
+  static targets = ["navbarMenuBtn"];
+
+  toggle() {
+    this.navbarMenuBtnTarget.classList.toggle("d-none");
+  }
+}
