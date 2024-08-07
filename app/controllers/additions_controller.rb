50bfcf849aa76e72724807317e21e9d7b7b77d01
Barry Roberts <barryjroberts@gmail.com>
2024-08-07 18:58:26 +0900

    changed updated movies view

--- app/controllers/additions_controller.rb
+++ app/controllers/additions_controller.rb
@@ -1,6 +1,7 @@
 class AdditionsController < ApplicationController
   def index
     @latest_additions = Movie.where("created_at >= ?", 1.hour.ago).order(created_at: :desc)
-    @recent_additions = Movie.order(created_at: :desc).limit(20)
+    # @recent_additions = Movie.where("created_at.day >= ?", (Movie.last.created_at.day))
+    @recent_additions = Movie.where("created_at >= ?", Movie.maximum(:created_at).beginning_of_day)
   end
 end
