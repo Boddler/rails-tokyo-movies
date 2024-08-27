5ae530920837959032f4862c60e41a9243f7bff7
Barry Roberts <barryjroberts@gmail.com>
2024-08-27 22:42:35 +0900

    nearly there

---
+++ app/javascript/controllers/tom_select_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "@hotwired/stimulus";
+import * as TomSelectModule from "tom-select";
+export default class extends Controller {
+  connect() {
+    console.log(12);
+
+    new TomSelect(this.element, {
+      sortField: 'text',
+      create: false,
+      onItemAdd: function(value, item) {
+        // Clear the input field after selecting an item
+        this.setTextboxValue('');
+      }
+    });
+  }
+}
