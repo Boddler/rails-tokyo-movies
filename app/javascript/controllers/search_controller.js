1763fb86af5950ebeb83f5e85106c05ec0e8d456
Barry Roberts <barryjroberts@gmail.com>
2024-08-29 13:42:31 +0900

    search OK now

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,8 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="search"
 export default class extends Controller {
 static targets = ["input", "cinema", "form"];
+
+connect() {
+  console.log("search controller connected");
+
+}
 
   cinema(event) {
     const checkbox = event.target;
